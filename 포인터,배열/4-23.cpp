35e3ddf2b7b3fd1d2a3053e3b23d87143856bcd8
geniehyeon31 <hgenie3113@gmail.com>
2022-08-12 16:04:08 +0900

    812

---
+++ 포인터,배열/4-23.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *pi = new int;
+
+    *pi = 100;
+
+    cout << *pi << endl;
+    
+    delete pi;
+
+    int *pj = new int[3];
+    int i;
+
+    pj[0]=10;
+    pj[1]=
+}
